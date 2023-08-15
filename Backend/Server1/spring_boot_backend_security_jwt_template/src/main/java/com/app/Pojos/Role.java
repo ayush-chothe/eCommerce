a407c2dc4d2644458bb49dd588b1a5692811abd9
Ayush Chothe <ayushchothe@gmail.com>
2023-08-15 20:37:13 +0530

    added 3 pojos

---
+++ Backend/Server1/spring_boot_backend_security_jwt_template/src/main/java/com/app/Pojos/Role.java
@@ -0,0 +1,5 @@
+package com.app.Pojos;
+
+public enum Role {
+	CUSTOMER, SELLER, ADMIN
+}
---
+++ Backend/Server1/spring_boot_backend_security_jwt_template/src/main/java/com/app/Pojos/User.java
@@ -0,0 +1,35 @@
+package com.app.Pojos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users") 
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = false, doNotUseGetters = true,of = "email")
+public class User extends BaseEntity{
+
+	@Column(length = 30)
+	private String name;
+	@Column(length = 30, unique = true) // =>unique
+	private String email;
+	@Column(nullable = false, length=30) // =>NOT NULL
+	private String password;		
+	@Enumerated(EnumType.STRING) // col : varchar => enum constant name
+	@Column(length = 10)
+	private Role role;
+	@Column(length = 10)
+	private String mobile;
+}
