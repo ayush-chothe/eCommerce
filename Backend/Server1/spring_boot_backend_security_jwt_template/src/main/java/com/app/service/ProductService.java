553c3285338260f758090b93bd9379f704668e51
Ayush Chothe <ayushchothe@gmail.com>
2023-08-18 18:23:55 +0530

    added image upload and download

--- Backend/Server1/spring_boot_backend_security_jwt_template/src/main/java/com/app/service/ProductService.java
+++ Backend/Server1/spring_boot_backend_security_jwt_template/src/main/java/com/app/service/ProductService.java
@@ -1,5 +1,9 @@
 package com.app.service;
 
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.app.dto.ApiResponse;
 import com.app.dto.ProductDTO;
 
@@ -7,4 +11,7 @@ public interface ProductService {
 
 	ApiResponse addNewProduct(ProductDTO productDto);
 	ProductDTO getProduct(Long id);
+	ApiResponse deleteProduct(Long id);
+	ApiResponse addImage(Long productId, MultipartFile image) throws IOException;
+	public byte[] getImages(Long productId) throws IOException;
 }
--- Backend/Server1/spring_boot_backend_security_jwt_template/src/main/java/com/app/service/ProductServiceImpl.java
+++ Backend/Server1/spring_boot_backend_security_jwt_template/src/main/java/com/app/service/ProductServiceImpl.java
@@ -1,16 +1,24 @@
 package com.app.service;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.app.dto.ApiResponse;
 import com.app.dto.ProductDTO;
 import com.app.pojo.Product;
+import com.app.pojo.ProductImage;
+import com.app.pojo.ProductStatus;
 import com.app.pojo.SubSubCategory;
 import com.app.pojo.User;
+import com.app.repository.ProductImageRepository;
 import com.app.repository.ProductRepository;
 import com.app.repository.SubSubCategoryRepository;
 import com.app.repository.UserRepository;
@@ -29,6 +37,9 @@ public class ProductServiceImpl implements ProductService {
 	private SubSubCategoryRepository subSubCategoryRepository;
 	
 	@Autowired
+	private ProductImageRepository productImageRepository;
+	
+	@Autowired
 	private ModelMapper mapper;
 
 	// add product
@@ -42,6 +53,7 @@ public class ProductServiceImpl implements ProductService {
 		
 		product.setSeller(seller);
 		product.setCategory(cat);
+		product.setStatus(ProductStatus.ADDED);
 		
 		productRepository.save(product);
 		
@@ -53,7 +65,31 @@ public class ProductServiceImpl implements ProductService {
 		Product product = productRepository.findById(id).orElseThrow();
 		return mapper.map(product, ProductDTO.class);
 	}
+
+	@Override
+	public ApiResponse deleteProduct(Long id) {
+		Product product = productRepository.findById(id).orElseThrow();
+		product.setStatus(ProductStatus.REMOVED);
+		return new ApiResponse("Product Removed");
+	}
+
+	@Override
+	public ApiResponse addImage(Long productId, MultipartFile image) throws IOException {
+		ProductImage productImage = new ProductImage();
+		Product product = productRepository.findById(productId).orElseThrow();
+		productImage.setProduct(product);
+		productImage.setImage(image.getBytes());
+		
+		productImageRepository.save(productImage);
+		
+		return new ApiResponse("Image added successfully");
+	}
 	
-	
+	@Override
+	public byte[] getImages(Long productImageId) throws IOException {
+
+		ProductImage productImage = productImageRepository.findById(productImageId).orElseThrow();
+		return productImage.getImage();
+	}
 
 }
